9686c9cebd47ed143be9edcbd0acc108e9ec51e9
thorraythorray <helloleidong@gmail.com>
2023-10-22 22:50:50 +0800

    add: patient mgr moudle

--- settings.py
+++ settings.py
@@ -170,7 +170,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-        'apps.admin.permissons.CasbinAuthenticated'
+        # 'apps.admin.permissons.CasbinAuthenticated'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
